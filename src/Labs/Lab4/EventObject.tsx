9f032760cb508ce6c86ae71940cf2ab19c013f71
Kushal-Shankar-1 <kushalshankar03@gmail.com>
2024-10-31 15:22:25 -0400

    Updated EventObject

--- src/Labs/Lab4/EventObject.tsx
+++ src/Labs/Lab4/EventObject.tsx
@@ -1,35 +1,20 @@
 import React, { useState } from "react";
-
 export default function EventObject() {
-  const [eventData, setEventData] = useState<any>(null);
-
+  const [event, setEvent] = useState(null);
   const handleClick = (e: any) => {
-    // Extract specific properties we want to display
-    const { type, timeStamp, target } = e;
-
-    // Create a simplified event object for display
-    const simplifiedEvent = {
-      type,
-      timeStamp,
-      target: target.outerHTML, // Only use outerHTML for target
-    };
-
-    // Set the simplified event data
-    setEventData(simplifiedEvent);
+    e.target = e.target.outerHTML;
+    delete e.view;
+    setEvent(e);
   };
-
   return (
     <div>
       <h2>Event Object</h2>
-      <button
-        onClick={(e) => handleClick(e)}
+      <button onClick={(e) => handleClick(e)}
         className="btn btn-primary"
-        id="wd-display-event-obj-click"
-      >
+        id="wd-display-event-obj-click">
         Display Event Object
       </button>
-      <pre>{eventData ? JSON.stringify(eventData, null, 2) : "Click the button to display event data"}</pre>
-      <hr />
+      <pre>{JSON.stringify(event, null, 2)}</pre>
+      <hr/>
     </div>
-  );
-}
+);}
