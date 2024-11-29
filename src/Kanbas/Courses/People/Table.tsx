9f898bc271157e06b0ebfe808da39ac3f7d51222
Kushal-Shankar-1 <kushalshankar03@gmail.com>
2024-11-28 20:07:33 -0500

    Initial A6 assignment

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,20 +1,10 @@
-import { useParams } from "react-router-dom";
+import React from "react";
 import { FaUserCircle } from "react-icons/fa";
-import * as db from "../../Database";
+import { Link } from "react-router-dom";
 
-export default function PeopleTable() {
-  const { cid } = useParams<{ cid: string }>(); // Retrieve course ID from URL
-  const { users, enrollments } = db;
-
-  // Handle cases where users or enrollments might not be loaded correctly
-  const enrolledUsers = users.filter((user) =>
-    enrollments.some(
-      (enrollment) => enrollment.user === user._id && enrollment.course === cid
-    )
-  );
-
-  if (enrolledUsers.length === 0) {
-    return <p>No users enrolled in this course.</p>;
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
+  if (users.length === 0) {
+    return <p>No users found.</p>;
   }
 
   return (
@@ -31,12 +21,17 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {enrolledUsers.map((user) => (
+          {users.map((user) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName}</span>{" "}
-                <span className="wd-last-name">{user.lastName}</span>
+                <Link
+                  to={`/Kanbas/Account/Users/${user._id}`}
+                  className="text-decoration-none"
+                >
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">{user.firstName}</span>{" "}
+                  <span className="wd-last-name">{user.lastName}</span>
+                </Link>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
